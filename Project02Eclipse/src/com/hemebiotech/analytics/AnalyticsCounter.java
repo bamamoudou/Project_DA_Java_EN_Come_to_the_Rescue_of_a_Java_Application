15f943597abfca298a817137ee9852b522300aee
bamamoudou <mamoudouba8787@gmail.com>
2020-05-13 17:47:30 +0200

    implementation of the functionality that counts the symptoms

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,42 +2,35 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+  public static void main(String args[]) throws Exception {
+    // first get input
+    BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+    String line = reader.readLine();
+    TreeMap<String, Integer> counter = new TreeMap<>();
 
-			line = reader.readLine();	// get another symptom
-		}
-		reader.close();
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    while (line != null) {
+
+      System.out.println("symptom from file: " + line);
+      if (counter.containsKey(line)) {
+        counter.put(line, counter.get(line) + 1);
+      } else {
+        counter.put(line, 1);
+      }
+
+      line = reader.readLine(); // get another symptom
+    }
+    reader.close();
+    counter.forEach((key, value) -> System.out.println(key + ":" + value));
+
+//		// next generate output
+//		FileWriter writer = new FileWriter ("result.out");
+//		writer.write("headache: " + headacheCount + "\n");
+//		writer.write("rash: " + rashCount + "\n");
+//		writer.write("dialated pupils: " + pupilCount + "\n");
+//		writer.close();
+  }
 }
